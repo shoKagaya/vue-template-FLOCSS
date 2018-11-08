eae155a66db5e4ad310034fc778ead720aa68824
Sho Kagaya <s.sho.kagaya@iridge.jp>
2018-11-08 13:34:55 +0900

    update

--- src/pages/template.vue
+++ src/pages/template.vue
@@ -4,14 +4,14 @@
     <section class="p-award">
       <h2 class="c-sectionTitle">Award</h2>
       <div class="p-award__content u-mb40">Awardのコンテンツが入ります。</div>
-      <h2 class="c-font--MPLUS u-mb20">Font MPLUS</h2>
-      <h2 class="c-font--montserrat u-mb20">Font Montserrat</h2>
+      <h2 class="u-font--MPLUS u-mb20">Font MPLUS</h2>
+      <h2 class="u-font--montserrat u-mb20">Font Montserrat</h2>
     </section>
     <section class="p-mission">
       <h2 class="c-sectionTitle u-mb24">Mission</h2>
       <div class="p-mission__content u-mb40">Missionのコンテンツが入ります。</div>
-      <h2 class="c-font--MPLUS u-mb20">Font MPLUS</h2>
-      <h2 class="c-font--montserrat u-mb20">Font Montserrat</h2>
+      <h2 class="u-font--MPLUS u-mb20">Font MPLUS</h2>
+      <h2 class="u-font--montserrat u-mb20">Font Montserrat</h2>
     </section>
   </div>
 </template>
